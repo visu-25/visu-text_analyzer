16245e529342350c28da08331d174aa182a271a7
Vishwajit Parmar <vishwajit25401@gmail.com>
2022-08-25 17:35:56 +0530

    last

--- visu/settings.py
+++ visu/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure--qfa(-j$bkfxa8*$a7v43w*#n=p7az9@cdf8%-e#^)q=#k-hpw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["visu-text-analyzer.herokuapp.com","localhost"] 
+ALLOWED_HOSTS = ["visu-text-analyzer.herokuapp.com","localhost","*"] 
 # 'visu-text-analyzer.herokuapp.com'
 
 
